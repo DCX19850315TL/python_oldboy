da1108ee6c59dd805bc311d74dbdd48925481263
tanglei <tanglei_0315@163.com>
2017-10-09 18:06:58 +0800

    编写购物车程序_1

--- 2The second day/Exercises/shopping.py
+++ 2The second day/Exercises/shopping.py
@@ -3,3 +3,24 @@
 #要求用户输入工资，然后打印购物菜单
 #用户可以不断的购买商品，直到钱不够为止
 #退出时格式化打印用户已购买的商品和剩余金额
+#【第10课】11 python s8day2 购物车程序练习
+import sys
+import os
+
+salary = raw_input('输入工资:')
+print(salary,'元')
+commodity = {
+    'apple':100,
+    'banana':50,
+    'pear':40
+}
+print(commodity)
+while salary > commodity
+    buy = raw_input(购买一个:)
+    if
+else
+    print '您的工资无法购买任何一个商品'
+
+
+
+
--- 3The third day/Yield/yield.py
+++ 3The third day/Yield/yield.py
@@ -6,3 +6,13 @@
 @file: yield.py
 @time: 2017/9/29 15:21
 '''
+#yield的原理介绍
+'''
+def foo():
+    yield 1
+re = foo()
+print re
+'''
+
+print range(10)
+print xrange(10)
